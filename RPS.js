62d39489b1d51beb248c3942f438a05eed51f8fa
SHEEPDOG0351 <michaelwork2006@gmail.com>
2024-09-30 09:54:30 -0400

    Fixed functions

--- RPS.js
+++ RPS.js
@@ -1,7 +1,5 @@
 const prompt = require('prompt-sync')()
-
-let user = prompt('Choose either rock, paper, or scissors: ')
-
+let points = 0
 
 let flag = true
 
@@ -24,10 +22,9 @@ function generateComputerDecision() {
 
 function rock_paper_scissors() {
     while (flag) {
-        let points = 0
         let user = prompt('Select either rock, paper, or scissors: ')
-        if (user.lower() == 'rock') {
-            let computer_decision = generateComputerDecision()
+        let computer_decision = generateComputerDecision()
+        if (user.toLowerCase() == 'rock') {
             if (computer_decision == 'paper') {
                 points--
                 console.log(`You lost this round as the computer chose ${computer_decision} and lose a point. You currently have ${points} points.`)
@@ -36,31 +33,31 @@ function rock_paper_scissors() {
                 console.log(`You both selected rock! This round is a tie. You currently have ${points} points.`)
             } else if (computer_decision == 'scissors') {
                 points++
-                console.log(`You won this round as you have selected ${user} and gain a point! You currently have ${points} points!`)
+                console.log(`You won this round as you have selected ${user} and the computer seclected ${computer_decision}, thus you gain a point! You currently have ${points} points!`)
             }
         }
-        else if (user.lower() == 'paper') {
+        else if (user.toLowerCase() == 'paper') {
             let computer_decision = generateComputerDecision()
             if (computer_decision == 'rock') {
                 points++
-                console.log(`You won this round as you have selected ${user} and gain a point! You currently have ${points} points!`)
+                console.log(`You won this round as you have selected ${user} and the computer seclected ${computer_decision}, thus you gain a point! You currently have ${points} points!`)
             } else if (computer_decision == 'paper') {
                 console.log(`You both selected paper! This round is a tie. You currently have ${points} points.`)
             } else if (computer_decision == 'scissors') {
                 points--
                 console.log(`You lost this round as the computer chose ${computer_decision} and lose a point. You currently have ${points} points.`)
             }
-        } else if (user.lower() == 'scissors') {
+        } else if (user.toLowerCase() == 'scissors') {
             let computer_decision = generateComputerDecision()
             if (computer_decision == 'rock') {
                 points--
                 console.log(`You lost this round as the computer chose ${computer_decision} and lose a point. You currently have ${points} points.`)
             } else if (computer_decision == 'paper') {
                 points++
-                console.log(`You won this round as you have selected ${user} and gain a point! You currently have ${points} points!`)
+                console.log(`You won this round as you have selected ${user} and the computer seclected ${computer_decision}, thus you gain a point! You currently have ${points} points!`)
             }
         }
-        else if (user.lower() == 'quit') {
+        else if (user.toLowerCase() == 'quit') {
             console.log(`You had ${points} points total!`)
             flag = false
         }
